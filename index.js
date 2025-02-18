828c7d6ab0b8b1b37432f1fec86209d8d9b59f8f
essam1212 <essam5ali2000@gmail.com>
2025-02-18 14:14:11 +0200

    update code

--- index.js
+++ index.js
@@ -1,40 +1,41 @@
-import express from "express";
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import connectDB from "./DB/connection.js";
-import * as indexRouter from './src/index.router.js';
+    import express from "express";
+    import bodyParser from 'body-parser';
+    import cors from 'cors';
+    import dotenv from 'dotenv';
+    import connectDB from "./DB/connection.js";
+    import * as indexRouter from './src/index.router.js';
 
-dotenv.config();
+    dotenv.config();
+    console.log("Base URL:", process.env.BASEURL);
 
-export const app = express();
-const PORT = process.env.PORT||3000 ;
-const baseUrl = process.env.BASEURL;
+    export const app = express();
+    const PORT = process.env.PORT||3000 ;
+    const baseUrl = process.env.BASEURL;
 
-app.use(cors());
-app.use(bodyParser.json());
-// home 
-app.get('/', (req, res) => { res.json({ message: 'welcome to our api' }) });
-// api auth
-app.use(`${baseUrl}/auth/student`, indexRouter.authStudentRouter);
-app.use(`${baseUrl}/auth/teacher`, indexRouter.authTeacherRouter);
-app.use(`${baseUrl}/auth/assistant`, indexRouter.authAssistantRouter);
-// api study
-app.use(`${baseUrl}/study/schoolYear`, indexRouter.schoolYearRouter);
-app.use(`${baseUrl}/study/lesson`, indexRouter.lessonRouter);
-app.use(`${baseUrl}/study/chapter`, indexRouter.chapterRouter);
-app.use(`study/subject`, indexRouter.subjectRouter);
-app.use(`${baseUrl}/study/exam`, indexRouter.examRouter);
-// api display
-app.use(`${baseUrl}/display/students`, indexRouter.displayStudents);
-app.use(`${baseUrl}/display/teachers`, indexRouter.displayTeacher);
-app.use(`${baseUrl}/display/assistants`, indexRouter.displayAssistant);
+    app.use(cors());
+    app.use(bodyParser.json());
+    // home 
+    app.get('/', (req, res) => { res.json({ message: 'welcome to our api' }) });
+    // api auth
+    app.use(`${baseUrl}/auth/student`, indexRouter.authStudentRouter);
+    app.use(`${baseUrl}/auth/teacher`, indexRouter.authTeacherRouter);
+    app.use(`${baseUrl}/auth/assistant`, indexRouter.authAssistantRouter);
+    // api study
+    app.use(`${baseUrl}/study/schoolYear`, indexRouter.schoolYearRouter);
+    app.use(`${baseUrl}/study/lesson`, indexRouter.lessonRouter);
+    app.use(`${baseUrl}/study/chapter`, indexRouter.chapterRouter);
+    app.use('/api/v1/study/subject', indexRouter.subjectRouter);
+    app.use(`${baseUrl}/study/exam`, indexRouter.examRouter);
+    // api display
+    app.use(`${baseUrl}/display/students`, indexRouter.displayStudents);
+    app.use(`${baseUrl}/display/teachers`, indexRouter.displayTeacher);
+    app.use(`${baseUrl}/display/assistants`, indexRouter.displayAssistant);
 
-connectDB();
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+    connectDB();
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
 
-export default app;
+    export default app;
 
- 
+    
