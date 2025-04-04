6e807d4c8a6b8ea6df23adaf88756b772dd0dd7d
essam1212 <essam5ali2000@gmail.com>
2025-04-04 13:43:28 +0200

    Your commit message

--- DB/model/code.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const codeSchema = new mongoose.Schema({
-    code: { type: String, required: true, unique: true },
-    expiresAt: { type: Date, required: true },
-  });
-  
-  export const AccessCode  = mongoose.model('code', codeSchema);
--- src/utils/emailService.js
+++ src/utils/emailService.js
@@ -1,25 +1,31 @@
 import nodemailer from 'nodemailer';
+// إعداد Sendinblue مع Nodemailer
+const transporter = nodemailer.createTransport({
+  host: 'smtp-relay.sendinblue.com',
+  port: 587,
+  secure: false, // لازم تكون false لاستخدام TLS
+  auth: {
+    user: process.env.SENDINBLUE_EMAIL, // البريد الإلكتروني الخاص بك في Sendinblue
+    pass: process.env.SENDINBLUE_API_KEY, 
+  },
+});
 export const sendEmail = async (to, subject, htmlContent) => {
     try{
-        const transporter =nodemailer.createTransport({
-            service:"gmail",
-            auth:{
-                user:process.env.nodeMailerEmail,
-                pass:process.env.nodeMailerPassword
-            }
-        })
-        const mailOptions = {
-            from: process.env.nodeMailerEmail,
-            to,
-            subject,
-            html: htmlContent
-          };
-          await transporter.sendMail(mailOptions);
-          console.log('Email sent successfully');
-        } catch (error) {
-          console.error('Error sending email:', error);
-        }
+      const info=await transporter.sendMail({
+        from: process.env.SENDINBLUE_EMAIL, // البريد الإلكتروني الخاص بك في Sendinblue
+        to, // البريد الإلكتروني المستلم
+        subject, // موضوع البريد الإلكتروني
+        html: htmlContent, // محتوى البريد الإلكتروني
+      });
+      console.log('✅ Email sent:', info.messageId);
+      return true;
+
+
+    } catch (error) {
+      console.error('❌ Error sending email:', error);
+      return false;
       };
+    }
       
       
     
