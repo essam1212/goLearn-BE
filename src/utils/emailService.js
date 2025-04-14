35622e74b3beb1cec996f26ca5d09ddffbb11e73
essam1212 <essam5ali2000@gmail.com>
2025-04-14 14:28:04 +0200

    update code

--- src/utils/emailService.js
+++ src/utils/emailService.js
@@ -1,6 +1,7 @@
 // utils/sendEmail.js
 import SibApiV3Sdk from 'sib-api-v3-sdk';
 
+
 const sendEmail = async (to, subject, htmlContent) => {
   const client = SibApiV3Sdk.ApiClient.instance;
   const apiKey = client.authentications['api-key'];
@@ -9,12 +10,13 @@ const sendEmail = async (to, subject, htmlContent) => {
   const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
 
   const emailData = {
-    sender: { name: 'اسم المنصة', email: 'essam5ali2000@gmail.com' }, 
+    sender: { name: 'Go Learn', email: 'essam5ali2000@gmail.com' },
+    to: [{ email: to }],
     subject,
     htmlContent,
   };
 
-  return apiInstance.sendTransacEmail(x);
+  return apiInstance.sendTransacEmail(emailData);
 };
 
 export default sendEmail;
